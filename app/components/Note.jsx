47b79dffadfad48c6d0a24419e3037c34a5b4c03
Jerell Robinson <jrobinson@nexient.com>
2016-05-23 11:58:51 -0400

    structure refactoring

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -34,7 +34,7 @@ export default class Note extends React.Component {
 
     return (
     <div onClick={this.edit}>
-      <span>{this.props.task}</span>
+      <span className="task">{this.props.task}</span>
       {onDelete ? this.renderDelete() : null }
     </div>
   );
